13e5018b5ae6215a697fac0a4f0b03567f09f4a0
tsalgie <16627886+tsalgie@users.noreply.github.com>
2022-12-02 21:14:27 -0600

    days 1 and 2

---
+++ 01/puzzle.py
@@ -0,0 +1,11 @@
+from typing import List
+
+def puzzle(calories: List[int], elf_count: int = 1):
+    return sum(sorted(calories)[-elf_count:])
+
+if __name__ == "__main__":
+    with open('input.txt') as f:
+        calories = [sum(map(int, group.split('\n'))) for group in f.read().split('\n\n')]
+
+    print(puzzle(calories))
+    print(puzzle(calories, 3))
---
+++ 02/puzzle.py
@@ -0,0 +1,43 @@
+from typing import List
+
+beat = {'X': 'C', 'Y': 'A', 'Z': 'B'}
+tie = {'A': 'X', 'B': 'Y', 'C': 'Z'}
+
+def puzzle1(matches: List[str]):
+    scores = {'X': 1, 'Y': 2, 'Z': 3}    
+    results = 0
+
+    for match in matches:
+        total = 0
+        if tie[match[0]] == match[1]:
+            total = 3
+        elif beat[match[1]] == match[0]:
+            total = 6
+
+        results += total + scores[match[1]]
+
+    return(results)
+
+def puzzle2(matches: List[str]):
+    beat = {'C': 'X', 'A': 'Y', 'B': 'Z'}
+    tie = {'A': 'X', 'B': 'Y', 'C': 'Z'}
+    lose = {'A': 'Z', 'B': 'X', 'C': 'Y'}
+
+    matches_copy = []   
+    for match in matches:
+        if match[1] == 'X':
+            match[1] = lose[match[0]]
+        elif match[1] == 'Y':
+            match[1] = tie[match[0]]
+        else:
+            match[1] = beat[match[0]]
+        matches_copy.append(match)
+
+    return(puzzle1(matches_copy))
+
+if __name__ == "__main__":
+    with open('input.txt') as f:
+        matches = [line.split() for line in f.readlines()]
+
+    print(puzzle1(matches))
+    print(puzzle2(matches))
